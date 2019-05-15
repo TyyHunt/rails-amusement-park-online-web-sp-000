d2c0d75f81111c08b9f338d40e4f4e61ce126432
Tyler Hunt <tyhunt04@gmail.com>
2019-05-15 16:00:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :rides
-  resources :attractions
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :rides, only: [:new, :show, :create]
+  resources :attractions, only: [:new, :show, :create, :index, :edit, :update]
+  resources :users, only: [:new, :show, :create]
+
+  root :to => 'users#new'
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sesions#create'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
 end
